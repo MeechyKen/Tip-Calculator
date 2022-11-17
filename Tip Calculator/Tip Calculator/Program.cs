19355df2bfcc7eaea1865e9215042134b1a9d223
Ismael Gonzalez <118404664+MeechyKen@users.noreply.github.com>
2022-11-16 23:36:17 -0600

    Add files via upload

---
+++ Tip Calculator/Tip Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿//Code Credit from Build a Tip Calculator
+
+//double billTotal = 30;
+
+//double tipPercentage = 15;
+
+// double tip = 4.5;
+
+//Console.WriteLine("Please enter the bill total:");
+
+//billTotal = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Please enter the tip percentage as a decimal (e.g., 15% would be entered as .15):");
+
+//tipPercentage = Convert.ToDouble(Console.ReadLine());
+
+//tip = billTotal * tipPercentage;
+
+//Console.WriteLine($"The {tipPercentage} tip for a bill of {billTotal} is {(double)4.5}.");
+
+//Console.ReadKey();
